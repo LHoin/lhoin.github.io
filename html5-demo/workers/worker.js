724f6f63248a8717c397b734285ded3c643a22c8
hongyang.liu <hongyang.liu@qunar.com>
2015-04-14 16:16:51 +0800

    add message

--- html5-demo/workers/worker.js
+++ html5-demo/workers/worker.js
@@ -1,6 +1,7 @@
 var arr = [];
-for (var i = 0; i<10000000; i++) {
+for (var i = 0; i<100000000; i++) {
 	arr.push(Math.round(Math.random() * 1000000));
 }
+postMessage('prepared');
 arr.sort();
 postMessage('finished');
